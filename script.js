0d21219ff9b21bbc617eac35e5f022f7bfded89d
HARISH04SS <113627438+HARISH04SS@users.noreply.github.com>
2024-04-20 12:17:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function displayDetails() {
+    var firstName = document.getElementById("firstName").value;
+    var middleName = document.getElementById("middleName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+    var address = document.getElementById("address").value;
+    
+    console.log("First Name:", firstName);
+    console.log("Middle Name:", middleName);
+    console.log("Last Name:", lastName);
+    console.log("Email:", email);
+    console.log("Address:", address);
+}
